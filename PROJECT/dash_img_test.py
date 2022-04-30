b4d8e1c562b1c6c4ce6d2713c5593a8a25165da2
nehat312 <nate.ehat@gmail.com>
2022-04-30 18:30:49 -0400

    Update dash_img_test.py

--- PROJECT/dash_img_test.py
+++ PROJECT/dash_img_test.py
@@ -1,4 +1,9 @@
 #%%
+
+# https://github.com/plotly/dash/issues/71
+
+
+
 import dash
 import dash_html_components as html
 import base64
@@ -6,7 +11,7 @@ import base64
 app = dash.Dash()
 
 
-image_filename = '/NBA-logo.png'
+image_filename = '/Users/nehat312/GitHub/Complex-Data-Visualization-/project/data/NBA-logo.png'
 encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 app.layout = html.Div([
@@ -14,6 +19,6 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=8050)
 
 #%%
